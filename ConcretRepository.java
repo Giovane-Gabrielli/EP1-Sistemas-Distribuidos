de1da06e748430d0676c108b717f3141e7625ea4
Giovane-Gabrielli <g.gabrielli512@gmail.com>
2017-04-17 17:13:39 -0300

    Add files via upload
    classe concreta que implementa um repositório de peças;

---
+++ ConcretRepository.java
@@ -0,0 +1,11 @@
+//classe concreta que implementa um repositório de peças;
+public class ConcretRepository implements PartRepository{
+	ArrayList lista = new ArrayList(); //coleçao de Parts;
+	//metodo de inserir peças ao repositorio:
+	public void inserePeca(String nome, String descricao, int[] sub){
+		//cria um objeto "Part":
+		
+		//insere o objeto na lista:
+		
+	}
+}
