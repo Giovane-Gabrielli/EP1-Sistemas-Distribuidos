7ac2cee47d3bac904e4b49de4019540d2f1a77ca
Giovane-Gabrielli <g.gabrielli512@gmail.com>
2017-04-17 17:10:53 -0300

    Update Part.java
    Classe Abstrata Part que representa uma peça;

--- Part.java
+++ Part.java
@@ -1,14 +1,9 @@
-//Essa é a classe que representa a "Component", seguindo o padrão de projeto "Composite":
+//classe abstrata Part que representa uma peça;
 public abstract class Part{
 	public int codPeca; // Código da peça, gerado automaticamente pelo sistema;
 	public String nome; // Nome da peça;
 	public String descricao; // Descrição da peça;
-	public Part  //lista de subcomponentes da peça (subPart, quantidade);
-	//Como fazer essa lista de subcomponentes?? já que subPart seria um objeto e quantidade seria um int???
-	
-	//metodo abstrato para adicionar uma peça:
-	public void adicionarPeca(Part peca){
-	}
+	public int[] sub = new int[2];  //lista de subcomponentes da peça (subPart, quantidade) - subPart pode conter apenas o identificador da subpeça;
 	
 	//geters:
 	public int getCodPeca(){
