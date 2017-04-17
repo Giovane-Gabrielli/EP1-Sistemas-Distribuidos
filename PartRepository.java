781d3cb7f89aa7d0cd93711426e3f38e398bcd69
Giovane-Gabrielli <g.gabrielli512@gmail.com>
2017-04-17 17:11:49 -0300

    Add files via upload
    interface que representa um repositório de peças;

---
+++ PartRepository.java
@@ -0,0 +1,10 @@
+//interface que representa um repositório de peças, deve extender "Remote";
+import java.util.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+public abstract class PartRepository extends Remote{
+
+	//metodo de inserir peças ao repositorio:
+	public void inserePeca(String nome, String descricao, int[] sub){
+	}
+}
